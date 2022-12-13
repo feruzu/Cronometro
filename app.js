d78781c25c5468c3300ed98933ba5fb9995a5cfc
feruzu <fernanda23demarzo@gmail.com>
2022-12-13 11:51:47 -0300

    Funciones

--- app.js
+++ app.js
@@ -1,24 +1,54 @@
-let botonStart = document.querySelector('.start');
-let botonPause = document.querySelector('.pause');
-let botonReset = document.querySelector('.reset');
+let botonStart = document.querySelector(".start");
+let botonPause = document.querySelector(".pause");
+let botonReset = document.querySelector(".reset");
 
-botonStart.addEventListener('click', start);
-botonPause.addEventListener('click', pause);
-botonReset.addEventListener('click', reset);
+let hora = (min = seg = "0" + 0),
+  startTiempo;
 
+botonStart.addEventListener("click", start);
+botonPause.addEventListener("click", pause);
+botonReset.addEventListener("click", reset);
 
 function start() {
-    botonStart.classList.add('active');
-    botonStart.classList.remove('stopActive');
-    botonPause.classList.remove('stopActive');
+  botonStart.classList.add("active");
+  botonStart.classList.remove("stopActive");
+  botonPause.classList.remove("stopActive");
+
+  startTiempo = setInterval(() => {
+    seg++;
+    seg = seg < 10 ? "0" + seg : seg;
+
+    if (seg === 60) {
+      min++;
+      min = min < 10 ? "0" + min : min;
+      seg = "0" + 0;
+    }
+    if (min === 60) {
+      hora++;
+      hora = hora < 10 ? "0" + hora : hora;
+      min = "0" + 0;
+    }
+    cronometro();
+  }, 1000);
 }
 
 function pause() {
-    botonStart.classList.remove('active');
-    botonPause.classList.add('stopActive');
+  botonStart.classList.remove("active");
+  botonPause.classList.add("stopActive");
+
+  clearInterval(startTiempo);
 }
 
 function reset() {
-    botonStart.classList.remove('active');
-    botonPause.classList.remove('stopActive');
+  botonStart.classList.remove("active");
+  botonPause.classList.remove("stopActive");
+
+  clearInterval(startTiempo);
+  (hora = min = seg = "0" + 0), cronometro();
+}
+
+function cronometro() {
+  document.querySelector(".seg").innerText = seg;
+  document.querySelector(".min").innerText = min;
+  document.querySelector(".hora").innerText = hora;
 }
